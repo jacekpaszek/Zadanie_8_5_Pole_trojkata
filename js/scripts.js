53616e894fbb228b8d31de9be9bc68b8a200ba2e
Jacek <jacek.paszek@onet.pl>
2017-06-08 21:34:43 +0200

    error handling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 var a = prompt('Enter base of triangle'),
-	h = prompt('Enter height of triangle');
+    h = prompt('Enter height of triangle');
 
-var triangleArea = a * h/2; //triangle area calculation
+var triangleArea = a * h / 2; //triangle area calculation
 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+if (triangleArea == NaN) {
+  console.log('Triangle parameters were wrong');
+} else if (triangleArea == 0) {
+  console.log('Triangle field is equal to: ' + 0 + ' Triangle parameters were wrong');
+} else {
+  console.log('Triangle field with base a: ' + a + ' and height h: ' + h
++ ' is equal to:     ' + triangleArea);
+}
+
