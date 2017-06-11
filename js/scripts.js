6d9494fc28abe7da97e9e82d6680396a3487a2a6
Jacek <jacek.paszek@onet.pl>
2017-06-11 21:16:31 +0200

    triangle parameters checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
 var a = prompt('Enter base of triangle'),
     h = prompt('Enter height of triangle');
 
-var triangleArea = a * h / 2; //triangle area calculation
-
-if (triangleArea == NaN) {
-  console.log('Triangle parameters were wrong');
-} else if (triangleArea == 0) {
-  console.log('Triangle field is equal to: ' + 0 + ' Triangle parameters were wrong');
+if ((isNaN(a) || isNaN(h)) || ((a <= 0) || (h <= 0))) {
+  console.log('Triangle parameters were wrong'); //triangle parameters checking
 } else {
+  var triangleArea = a * h / 2;                  //triangle area calculation
   console.log('Triangle field with base a: ' + a + ' and height h: ' + h
-+ ' is equal to:     ' + triangleArea);
++ ' is equal to: ' + triangleArea);
 }
-
